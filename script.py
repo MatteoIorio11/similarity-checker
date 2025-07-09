78edf8ebe1dda16aaa7b1f6d312cfa3b0689be02
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-09 12:38:44 +0200

    feat: read images and use them for similarity

--- script.py
+++ script.py
@@ -1,20 +1,29 @@
-import opencv as cv
-from typing import List
-import numpy as n
+from typing import List, Set
+import os
+import numpy as np
 from gabor_strategy import GaborStrategy
 from strategy import Strategy
+import cv2 as cv
 
 gabor_strategy = GaborStrategy()
 
 
 def check_similarity(images: List[np.ndarray], strategy: Strategy) -> float:
     for image in images:
-        assert image != None
-    print(strategy.get_similarity(images))
-    return 0.0
+        assert image is not None
+    return strategy.get_similarity(images)
+
 
 def main():
-    print("Hello from tool!")
+    images: List[np.ndarray] = read_images()
+    score: float = check_similarity(images, gabor_strategy)
+    print(f"Similarity {score}")
+
+
+def read_images() -> List[np.ndarray]:
+    files: List[str] = os.listdir("./images")
+    image_format: Set[str] = {"jpg", "png", "jpeg"}
+    return [cv.imread(os.path.join("./images", file), cv.IMREAD_GRAYSCALE) for file in files if file.split(".")[1] in image_format]
 
 
 if __name__ == "__main__":
