a83c20625ebcb60e322df7010645f754e09f1d97
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-09 11:06:39 +0200

    feat: init similarity function, abstract away the similarity logic

--- script.py
+++ script.py
@@ -1,4 +1,11 @@
 import opencv as cv
+from typing import List, Callable
+import numpy as np
+
+def check_similarity(images: List[np.ndarray], strategy: Callable[[List[np.ndarray]], float]) -> float:
+    for image in images:
+        assert image != None
+    return strategy(images)
 
 def main():
     print("Hello from tool!")
