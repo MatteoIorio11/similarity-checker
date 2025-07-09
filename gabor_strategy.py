b8772c32a584c61b557ac9d454e7ff9adaf6ab56
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-09 12:38:31 +0200

    feat: implement the logic for cosin similarity, use arange instead of range

--- gabor_strategy.py
+++ gabor_strategy.py
@@ -1,9 +1,11 @@
+import random
 from typing import List, Tuple
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import normalize
 from strategy import Strategy
 
 import cv2 as cv
+import random
 import numpy as np
 
 
@@ -14,9 +16,9 @@ class GaborStrategy(Strategy):
         self.filters: List[np.ndarray] = self.generate_filters()
 
     def get_similarity(self, images: List[np.ndarray]) -> float:
-        scores: List[np.ndarray] = []
+        scores = []
         for image in images:
-            scores.append(normalize(self.similarity_for_image(cv.cvtColor(image, cv.COLOR_BGR2GRAY)))[0])
+            scores.append(normalize([self.similarity_for_image(image)])[0])
         return cosine_similarity([scores[0]], [scores[1]])[0][0]
 
     def similarity_for_image(self, image: np.ndarray) -> np.ndarray:
@@ -31,7 +33,8 @@ class GaborStrategy(Strategy):
         for y in range(0, height - patch_size + 1, step_y):
             for x in range(0, width - patch_size + 1, step_x):
                 patch: np.ndarray = image[y:y + patch_size, x:x + patch_size]
-                patch_features: List[float] = [cv.filter2D(patch, cv.CV_32F, fil) for fil in self.filters]
+                cv.imwrite(f"patch{random.randint(0, 100)}.jpg", patch)
+                patch_features: List[float] = [cv.filter2D(patch, cv.CV_32F, fil).mean() for fil in self.filters]
                 features.extend(patch_features)
         return np.array(features)
 
@@ -44,5 +47,5 @@ class GaborStrategy(Strategy):
         psi: int = 0
         return [cv.getGaborKernel((ksize, ksize), sigma, theta,
                                   glambda, gamma, psi, ktype=cv.CV_32F)
-                for theta in range(0, np.pi, np.pi / orientations)]
+                for theta in np.arange(0, np.pi, np.pi / orientations)]
 
